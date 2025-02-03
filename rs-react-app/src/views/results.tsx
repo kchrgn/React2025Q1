adaca0c46d666710ef38c7ac0d09d8521be3a1c8
unknown <kchrgn@gmail.com>
2025-02-03 14:09:29 +0300

    feat: start work whith results

--- rs-react-app/src/views/results.tsx
+++ rs-react-app/src/views/results.tsx
@@ -1,6 +1,14 @@
 import { Component } from 'react';
 
 class Results extends Component {
+  async componentDidMount(): Promise<void> {
+    const response = await fetch('https://swapi.dev/api/planets/');
+    if (response.ok) {
+      const json = await response.json();
+      const planets = json.results;
+      console.log(planets);
+    }
+  }
   render() {
     return (
       <div>
